10f56e5af51b3f802907871a3eaf029c3e9d0734
Minh Nguyen Cong <mcong@box.com>
2022-10-26 00:44:52 +0200

    Add Dockerfile

---
+++ Dockerfile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker build . -t cinemo-backend
+docker stop cinemo-backend; docker rm cinemo-backend;
+docker run -d -it --name cinemo-backend -p 127.0.0.1:9001:8080/tcp --restart always cinemo-backend
