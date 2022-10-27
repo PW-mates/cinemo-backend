668410b5c21cc1f4a0877627272eedb2565e573a
giangh14cqt <73480323+giangh14cqt@users.noreply.github.com>
2022-10-27 22:37:01 +0200

    Authentication 1 (#9)

--- src/main/java/com/pw/cinema/security/CustomAuthenticationFilter.java
+++ src/main/java/com/pw/cinema/security/CustomAuthenticationFilter.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -13,12 +14,16 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -32,11 +37,19 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
     }
 
     @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+    public Authentication attemptAuthentication(HttpServletRequest request_http, HttpServletResponse response) throws AuthenticationException {
         System.out.println("Request: ");
-        System.out.println(request);
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
+        JSONObject request;
+        try {
+            String str = request_http.getReader().lines().collect(Collectors.joining());
+            System.out.println(str);
+            request = new JSONObject(str);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String username = request.getString("username");
+        String password = request.getString("password");
         log.info("Username is: {}", username);
         log.info("Password is: {}", password);
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
@@ -48,10 +61,6 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         User user = (User)authentication.getPrincipal();
         Algorithm algorithm = Algorithm.HMAC256(System.getenv("JWT_SECRET").getBytes());
         String access_token = JWT.create().withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
-                .withIssuer(request.getRequestURL().toString())
-                .sign(algorithm);
-        String refresh_token = JWT.create().withSubject(user.getUsername())
                 .withExpiresAt(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000))
                 .withIssuer(request.getRequestURL().toString())
                 .sign(algorithm);
@@ -59,8 +68,26 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 //        response.setHeader("refresh_token", refresh_token);
         Map<String, String> tokens = new HashMap<>();
         tokens.put("access_token", access_token);
-        tokens.put("refresh_token", refresh_token);
         response.setContentType(APPLICATION_JSON_VALUE);
-        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+        Map<String, Object> resp = new HashMap<>();
+        resp.put("success:", "true");
+        resp.put("data", tokens);
+        resp.put("message", "Successful Authentication");
+        new ObjectMapper().writeValue(response.getOutputStream(), resp);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                              AuthenticationException failed) throws IOException {
+        this.logger.trace("Failed to process authentication request", failed);
+        this.logger.trace("Cleared SecurityContextHolder");
+        this.logger.trace("Handling authentication failure");
+        Map<String, String> resp = new HashMap<>();
+        resp.put("success", "false");
+        resp.put("message", "Unsuccessful Authentication");
+        resp.put("data", null);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.setStatus(UNAUTHORIZED.value());
+        new ObjectMapper().writeValue(response.getOutputStream(), resp);
     }
 }
