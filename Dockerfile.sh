66b1d733fe01fc3da3c6316bc95fd0a413acbe17
Minh Nguyen Cong <mcong@box.com>
2022-10-27 16:16:34 +0200

    Fix deploy workflow

--- Dockerfile.sh
+++ Dockerfile.sh
@@ -2,4 +2,4 @@
 
 docker build . -t cinemo-backend
 docker stop cinemo-backend; docker rm cinemo-backend;
-docker run -d -it --name cinemo-backend -p 127.0.0.1:9001:8080/tcp --restart always cinemo-backend
+docker run -d -it --name cinemo-backend -p 127.0.0.1:9001:8080/tcp --restart always --env-file ../.env-cinemo-backend cinemo-backend
