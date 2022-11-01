2c12da79a3ccae9f388822422a923137e101e4ca
Daria Komarynska <87605159+DariaKomarynska@users.noreply.github.com>
2022-11-01 01:52:14 +0100

    New kinds of exception, movie api (#15)
    Co-authored-by: Daria Komarynska <01165657@pw.edu.pl>

--- src/main/java/com/pw/cinema/exceptions/RestExceptionHandler.java
+++ src/main/java/com/pw/cinema/exceptions/RestExceptionHandler.java
@@ -2,14 +2,46 @@ package com.pw.cinema.exceptions;
 
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
 
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
-@ControllerAdvice()
+@RestControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoSuchElementException.class)
+    public Map<String, Object> handleNoSuchElementException(NoSuchElementException exception) {
+        return responseException(exception);
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public Map<String, Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException exception) {
+        return responseException(exception);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(AlreadyExistsException.class)
+    public Map<String, Object> handleAlreadyExistsException(AlreadyExistsException exception) {
+        return responseException(exception);
+    }
+
+    public Map<String, Object> responseException(Exception exception) {
+        Map<String, Object> errorMap = new HashMap<>();
+        errorMap.put("success", false);
+        errorMap.put("message", exception.getMessage());
+        return errorMap;
+    }
 
 
 }
--- src/main/java/com/pw/cinema/movie/MovieService.java
+++ src/main/java/com/pw/cinema/movie/MovieService.java
@@ -1,8 +1,12 @@
 package com.pw.cinema.movie;
 
-import lombok.extern.slf4j.Slf4j;
+import com.pw.cinema.exceptions.AlreadyExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class MovieService {
@@ -10,8 +14,54 @@ public class MovieService {
     @Autowired
     private MovieRepository movieRepository;
 
-    public Movie getMovieById(Long id) {
-        Movie movie = movieRepository.findById(id).orElse(null);
-        return movie;
+    public Object getMovieById(Long id) {
+        Movie movie = movieRepository.findById(id).get();
+        Map<String, Object> response = new HashMap<>();
+        response.put("data", movie);
+        response.put("message", "Successfully found movie");
+        response.put("success", true);
+        return response;
+    }
+
+    public Object create(Movie movie) throws AlreadyExistsException {
+        if (movieRepository.findByName(movie.getName()) != null) {
+            throw new AlreadyExistsException("Movie with that name already exists");
+        }
+        Map<String, Object> response = new HashMap<>();
+        response.put("data", movieRepository.save(movie));
+        response.put("message", "Successfully added new movie");
+        response.put("success", true);
+        return response;
+    }
+
+
+    public Object getMovieList() {
+        List<Movie> movies = movieRepository.findAll();
+        Map<String, Object> response = new HashMap<>();
+        response.put("data", movies);
+        response.put("message", "Successfully found movies");
+        response.put("success", true);
+        return response;
+    }
+
+    public Object updateMovie(Movie movieChanges, Long id) {
+        Movie movie = movieRepository.findById(id).get();
+        movie.setName(movieChanges.getName());
+        movie.setDescription(movieChanges.getDescription());
+        movie.setLength(movieChanges.getLength());
+        movie.setAgeRestriction(movieChanges.getAgeRestriction());
+        Map<String, Object> response = new HashMap<>();
+        response.put("data", movieRepository.save(movie));
+        response.put("message", "Successfully found movies");
+        response.put("success", true);
+        return response;
+    }
+
+    public Object deleteMovie(Long id) {
+        Map<String, Object> response = new HashMap<>();
+        movieRepository.deleteById(id);
+        response.put("message", "Successfully deleted movie");
+        response.put("success", true);
+        return response;
     }
 }
