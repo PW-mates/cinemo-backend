a5a7f9be8e1a7a292362f45ab1efe2a1c4cd4ad4
Truong Giang Do <giangh14cqt@gmail.com>
2022-11-01 21:16:53 +0100

    Add field for user

--- src/main/java/com/pw/cinema/user/User.java
+++ src/main/java/com/pw/cinema/user/User.java
@@ -17,10 +17,149 @@ public class User {
     @Column(unique = true)
     private String username;
     private String password;
+    Long birthDate;
+    String country;
+    String gender;
+    String bio;
+    String personalWebsite;
+    String language;
+    Long createdAt;
+    String email;
+    Long updatedAt;
+    String phone;
+    String profilePicture;
+    String status;
+    Long cinema;
     @ManyToMany(fetch = FetchType.EAGER)
     private Collection<Role> roles = new ArrayList<>();
 
     public User() {
+    }
+
+    public Long getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Long birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getPersonalWebsite() {
+        return personalWebsite;
+    }
+
+    public void setPersonalWebsite(String personalWebsite) {
+        this.personalWebsite = personalWebsite;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public Long getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Long updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getCinema() {
+        return cinema;
+    }
+
+    public void setCinema(Long cinema) {
+        this.cinema = cinema;
+    }
+
+    public User(Long id, String firstName, String lastName, String username, String password, Long birthDate, String country, String gender, String bio, String personalWebsite, String language, Long createdAt, String email, Long updatedAt, String phone, String profilePicture, String status, Long cinema, Collection<Role> roles) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.birthDate = birthDate;
+        this.country = country;
+        this.gender = gender;
+        this.bio = bio;
+        this.personalWebsite = personalWebsite;
+        this.language = language;
+        this.createdAt = createdAt;
+        this.email = email;
+        this.updatedAt = updatedAt;
+        this.phone = phone;
+        this.profilePicture = profilePicture;
+        this.status = status;
+        this.cinema = cinema;
+        this.roles = roles;
     }
 
     public User(Long id, String firstName, String lastName, String username, String password, Collection<Role> roles) {
@@ -68,6 +207,19 @@ public class User {
                 ", lastName='" + lastName + '\'' +
                 ", username='" + username + '\'' +
                 ", password='" + password + '\'' +
+                ", birthDate=" + birthDate +
+                ", country='" + country + '\'' +
+                ", gender='" + gender + '\'' +
+                ", bio='" + bio + '\'' +
+                ", personalWebsite='" + personalWebsite + '\'' +
+                ", language='" + language + '\'' +
+                ", createdAt=" + createdAt +
+                ", email='" + email + '\'' +
+                ", updatedAt=" + updatedAt +
+                ", phone='" + phone + '\'' +
+                ", profilePicture='" + profilePicture + '\'' +
+                ", status='" + status + '\'' +
+                ", cinema=" + cinema + '\'' +
                 ", roles=" + roles +
                 '}';
     }
