fc90420df79d04ebfd07622a7af56545f7e30c3d
Daria Komarynska <87605159+DariaKomarynska@users.noreply.github.com>
2022-11-01 01:52:14 +0100

    New kinds of exception, movie api (#15)
    Co-authored-by: Daria Komarynska <01165657@pw.edu.pl>

--- src/main/java/com/pw/cinema/movie/MovieController.java
+++ src/main/java/com/pw/cinema/movie/MovieController.java
@@ -1,23 +1,38 @@
 package com.pw.cinema.movie;
 
+import com.pw.cinema.exceptions.AlreadyExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/movies")
 public class MovieController {
 
     @Autowired
     private MovieService movieService;
 
-    @GetMapping
-    public ResponseEntity getMovieById(@RequestParam Long id) {
-        try {
-            return ResponseEntity.ok(movieService.getMovieById(id));
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Error: something wrong..");
-        }
+    @GetMapping(path = "/movie")
+    public ResponseEntity<Object> getMovieDetails(@RequestParam Long id) {
+        return ResponseEntity.ok().body(movieService.getMovieById(id));
     }
 
+    @GetMapping(path = "/movies")
+    public ResponseEntity<Object> getAllMovies() {
+        return ResponseEntity.ok().body(movieService.getMovieList());
+    }
+
+    @PostMapping(path = "/movies")
+    public ResponseEntity<Object> addMovie(@RequestBody Movie movie) throws AlreadyExistsException {
+        return ResponseEntity.ok().body(movieService.create(movie));
+    }
+
+    @PatchMapping(path = "/movie")
+    public ResponseEntity<Object> updateMovie(@RequestBody Movie movie, @RequestParam Long id) {
+        return ResponseEntity.ok().body(movieService.updateMovie(movie, id));
+    }
+
+    @DeleteMapping(path = "/movie")
+    public ResponseEntity<Object> deleteMovie(@RequestParam Long id) {
+        return ResponseEntity.ok().body(movieService.deleteMovie(id));
+    }
 }
