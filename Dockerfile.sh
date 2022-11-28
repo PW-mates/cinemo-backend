f4eb2e79a2afaa0aae6ffa72ad83f4af195e7f0c
Minh Nguyen Cong <clovers1254@gmail.com>
2022-11-28 02:36:52 +0100

    Setup docker artifacts (#26)

--- Dockerfile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build . -t cinemo-backend
-docker stop cinemo-backend; docker rm cinemo-backend;
-docker run -d -it --name cinemo-backend -p 127.0.0.1:9001:8080/tcp --restart always --env-file ../.env-cinemo-backend cinemo-backend
