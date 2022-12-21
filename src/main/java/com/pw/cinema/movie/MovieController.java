afa56e4b10a2a7ef83d0e6385959ba7885e208b3
Minh Nguyen Cong <clovers1254@gmail.com>
2022-12-21 22:35:11 +0100

    Revert "Movie relations (#28)"
    This reverts commit 87d1717442c76a7959245cb7afc589d6c81a0db6.

--- src/main/java/com/pw/cinema/movie/MovieController.java
+++ src/main/java/com/pw/cinema/movie/MovieController.java
@@ -11,41 +11,28 @@ public class MovieController {
     @Autowired
     private MovieService movieService;
 
+    @GetMapping(path = "/movie")
+    public ResponseEntity<Object> getMovieDetails(@RequestParam Long id) {
+        return ResponseEntity.ok().body(movieService.getMovieById(id));
+    }
+
     @GetMapping(path = "/movies")
     public ResponseEntity<Object> getAllMovies() {
         return ResponseEntity.ok().body(movieService.getMovieList());
     }
 
-    @PostMapping(path = "/movie")
-    public ResponseEntity<Object> createMovie(@RequestBody Movie movie) throws AlreadyExistsException {
+    @PostMapping(path = "/movies")
+    public ResponseEntity<Object> addMovie(@RequestBody Movie movie) throws AlreadyExistsException {
         return ResponseEntity.ok().body(movieService.create(movie));
     }
 
-    @PatchMapping(path = "/movies/:{id}")
-    ////// ID is param
-    public ResponseEntity<Object> updateMovie(@RequestBody Movie movie, @PathVariable Long id) {
+    @PatchMapping(path = "/movie")
+    public ResponseEntity<Object> updateMovie(@RequestBody Movie movie, @RequestParam Long id) {
         return ResponseEntity.ok().body(movieService.updateMovie(movie, id));
     }
 
-    @DeleteMapping(path = "/movies/:{id}")
-    ////// ID is param
-    public ResponseEntity<Object> deleteMovie(@PathVariable Long id) {
+    @DeleteMapping(path = "/movie")
+    public ResponseEntity<Object> deleteMovie(@RequestParam Long id) {
         return ResponseEntity.ok().body(movieService.deleteMovie(id));
     }
-
-    @GetMapping(path = "/movies/:{id}")
-    public ResponseEntity<Object> getMovie(@PathVariable Long id) {
-        return ResponseEntity.ok().body(movieService.getMovieById(id));
-    }
-
-    @PostMapping(path="/movies/:{id}/poster")
-    //  base64 encoded image
-    public ResponseEntity<Object> uploadPoster(@RequestBody String poster, @PathVariable Long id) {
-        return  ResponseEntity.ok().body(movieService.uploadPoster(poster, id));
-    }
-
-//    @PutMapping(path = "/{movieId}/categories/{categoryId}")
-//    public ResponseEntity<Object> addCategoryToMovie(@PathVariable Long movieId, Long categoryId) {
-//        return ResponseEntity.ok().body(movieService.addCategoryToMovie(movieId, categoryId));
-//    }
 }
