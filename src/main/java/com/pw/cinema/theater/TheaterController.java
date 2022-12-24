310bbb9aab77ddb668678895eb7195255b4e4c3f
Truong Giang Do <73480323+giangh14cqt@users.noreply.github.com>
2022-12-24 14:06:51 +0100

    Add get theaters (#37)

--- src/main/java/com/pw/cinema/theater/TheaterController.java
+++ src/main/java/com/pw/cinema/theater/TheaterController.java
@@ -3,6 +3,7 @@ package com.pw.cinema.theater;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,5 +15,10 @@ public class TheaterController {
     @PostMapping(path= "theaters")
     public ResponseEntity<Object> createTheater(@RequestBody Theater theater) {
         return ResponseEntity.ok().body(theaterService.createTheater(theater));
+    }
+
+    @GetMapping(path= "theaters")
+    public ResponseEntity<Object> getTheaters() {
+        return ResponseEntity.ok().body(theaterService.getTheaters());
     }
 }
