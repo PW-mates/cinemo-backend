3f08a840baef1172432e13933e7192ec4a9ec720
Truong Giang Do <giangh14cqt@gmail.com>
2022-12-26 15:13:38 +0100

    Add create room

--- src/main/java/com/pw/cinema/room/RoomController.java
+++ src/main/java/com/pw/cinema/room/RoomController.java
@@ -1,4 +1,19 @@
 package com.pw.cinema.room;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
 public class RoomController {
+    @Autowired
+    RoomService roomService;
+
+    @PostMapping(path="rooms")
+    public ResponseEntity<Object> createRoom(@RequestBody Room room) {
+        System.out.println("ok");
+        return ResponseEntity.ok().body(roomService.createRoom(room));
+    }
 }
