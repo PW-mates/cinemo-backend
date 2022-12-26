9a3b336780236419cda9ee455f5f5fdcc70bf4fb
Truong Giang Do <giangh14cqt@gmail.com>
2022-12-26 14:26:23 +0100

    Add get theater, fix room

--- src/main/java/com/pw/cinema/room/RoomRepository.java
+++ src/main/java/com/pw/cinema/room/RoomRepository.java
@@ -1,6 +1,10 @@
 package com.pw.cinema.room;
 
+import com.pw.cinema.theater.Theater;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoomRepository extends JpaRepository<Room, Long> {
+    List<Room> findAllByTheater(Theater theater);
 }
