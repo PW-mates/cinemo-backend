2781da984e2e62010f4b4693d7d9e1169227e4e4
Daria Komarynska <01165657@pw.edu.pl>
2022-12-31 02:16:03 +0200

    minimized response returning

--- src/main/java/com/pw/cinema/Utils.java
+++ src/main/java/com/pw/cinema/Utils.java
@@ -1,2 +1,16 @@
-package com.pw.cinema;public class Utils {
+package com.pw.cinema;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Utils {
+
+    public static Object response(Object result, String description) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("data", result);
+        response.put("message", description);
+        response.put("success", true);
+        return response;
+    }
+
 }
