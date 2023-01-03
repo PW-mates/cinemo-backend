c113615037b380b1c76fd02682a46cc820e4c031
Truong Giang Do <giangh14cqt@gmail.com>
2023-01-03 14:50:42 +0100

    Add delete room

--- src/main/java/com/pw/cinema/room/RoomController.java
+++ src/main/java/com/pw/cinema/room/RoomController.java
@@ -3,10 +3,7 @@ package com.pw.cinema.room;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RoomController {
@@ -26,5 +23,10 @@ public class RoomController {
     @GetMapping(path = "rooms/{id}")
     public ResponseEntity<Object> getRoom(@PathVariable("id") Long id) {
         return ResponseEntity.ok().body(roomService.getRoom(id));
+    }
+
+    @DeleteMapping(path = "rooms/{id}")
+    public ResponseEntity<Object> deleteRoom(@PathVariable("id") Long id) {
+        return ResponseEntity.ok().body(roomService.deleteRoom(id));
     }
 }
