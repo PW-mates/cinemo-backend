975cb794403c10bf6041b50ff64e297fd240a82e
Truong Giang Do <73480323+giangh14cqt@users.noreply.github.com>
2023-01-05 15:32:02 +0100

    Add update seat (#54)

--- src/main/java/com/pw/cinema/seat/SeatController.java
+++ src/main/java/com/pw/cinema/seat/SeatController.java
@@ -3,9 +3,7 @@ package com.pw.cinema.seat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class SeatController {
@@ -16,7 +14,12 @@ public class SeatController {
         return ResponseEntity.ok().body(seatService.getSeats());
     }
     @PostMapping(path = "seats")
-    public ResponseEntity<Object> createSeat(@PathVariable Seat seat) {
+    public ResponseEntity<Object> createSeat(@RequestBody Seat seat) {
         return ResponseEntity.ok().body(seatService.createSeat(seat));
+    }
+
+    @PatchMapping(path = "seats/{id}")
+    public ResponseEntity<Object> updateSeat(@PathVariable("id") Long id, @RequestBody Seat seat) {
+        return ResponseEntity.ok().body(seatService.updateSeat(id, seat));
     }
 }
