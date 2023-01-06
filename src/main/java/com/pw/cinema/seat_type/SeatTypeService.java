298ccb7c3efe0b92a80900f08b8d53d8123bf3d6
Truong Giang Do <giangh14cqt@gmail.com>
2023-01-06 20:47:02 +0100

    Add get SeatType endpoint

--- src/main/java/com/pw/cinema/seat_type/SeatTypeService.java
+++ src/main/java/com/pw/cinema/seat_type/SeatTypeService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -17,7 +18,16 @@ public class SeatTypeService {
         Map<String, Object> resp = new HashMap<>();
         resp.put("success", true);
         resp.put("data", savedSeatType);
-        resp.put("message", "Successful create theater");
+        resp.put("message", "Successful create seat type");
+        return resp;
+    }
+
+    public Object getSeatTypes() {
+        List<SeatType> savedSeatTypes = seatTypeRepository.findAll();
+        Map<String, Object> resp = new HashMap<>();
+        resp.put("success", true);
+        resp.put("data", savedSeatTypes);
+        resp.put("message", "Successful get seat type");
         return resp;
     }
 }
