fb3e62ea4e0c880885e8634028fe9097786220a4
Truong Giang Do <giangh14cqt@gmail.com>
2023-01-06 20:59:41 +0100

    Add field color, add in liquibase

--- src/main/resources/db/changelog/dummy_data/Basic-info.1.0.sql
+++ src/main/resources/db/changelog/dummy_data/Basic-info.1.0.sql
@@ -5,7 +5,7 @@
 
 INSERT INTO role VALUES (1,'ROLE_ADMIN'),(2,'ROLE_USER');
 
-INSERT INTO seat_type VALUES (1,'Classic'),(2,'VIP');
+INSERT INTO seat_type VALUES (1,'Classic','fefae0'),(2,'VIP','ffb703');
 
 INSERT INTO user VALUES
 (1,NULL,NULL,NULL,NULL,NULL,'Jack',NULL,NULL,'Daniel','$2a$10$90JM.duiCjHdXxHiAe2wpODQEs3IkU/tKMQp1KDKbtzK/HFexC9FW',NULL,NULL,NULL,NULL,NULL,'admin'),
