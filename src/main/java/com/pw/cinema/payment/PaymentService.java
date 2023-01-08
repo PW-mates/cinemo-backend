52ee0484a396ab02229991a9511be59fec172d35
Truong Giang Do <73480323+giangh14cqt@users.noreply.github.com>
2023-01-08 17:54:41 +0100

    Create payment endpoint (#64)

--- src/main/java/com/pw/cinema/payment/PaymentService.java
+++ src/main/java/com/pw/cinema/payment/PaymentService.java
@@ -3,8 +3,21 @@ package com.pw.cinema.payment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class PaymentService {
     @Autowired
     PaymentRepository paymentRepository;
+
+    public Object createPayment(Payment payment) {
+        payment.setId(null);
+        Payment savedPayment = paymentRepository.save(payment);
+        Map<String, Object> resp = new HashMap<>();
+        resp.put("success", true);
+        resp.put("data", savedPayment);
+        resp.put("message", "Successful create payment");
+        return resp;
+    }
 }
