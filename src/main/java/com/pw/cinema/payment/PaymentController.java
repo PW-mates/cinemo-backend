5edb72d8efdb358b552bde29814b78d487e60e96
Truong Giang Do <giangh14cqt@gmail.com>
2023-01-08 18:05:27 +0100

    Update payment

--- src/main/java/com/pw/cinema/payment/PaymentController.java
+++ src/main/java/com/pw/cinema/payment/PaymentController.java
@@ -3,10 +3,7 @@ package com.pw.cinema.payment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PaymentController {
@@ -26,5 +23,10 @@ public class PaymentController {
     @GetMapping(path = "payments/{id}")
     public ResponseEntity<Object> getPayment(@PathVariable("id") Long id) {
         return ResponseEntity.ok().body(paymentService.getPayment(id));
+    }
+
+    @PatchMapping(path = "payments/{id}")
+    public ResponseEntity<Object> updatePayment(@PathVariable("id") Long id, @RequestBody Payment payment) {
+        return ResponseEntity.ok().body(paymentService.updatePayment(id, payment));
     }
 }
