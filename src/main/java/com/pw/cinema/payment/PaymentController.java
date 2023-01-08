94fb0df48c5065833fa515daa0b3cb9e36c94d64
Truong Giang Do <73480323+giangh14cqt@users.noreply.github.com>
2023-01-08 18:01:14 +0100

    Add get payments (#65)

--- src/main/java/com/pw/cinema/payment/PaymentController.java
+++ src/main/java/com/pw/cinema/payment/PaymentController.java
@@ -3,6 +3,8 @@ package com.pw.cinema.payment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,5 +16,15 @@ public class PaymentController {
     @PostMapping(path = "payments")
     public ResponseEntity<Object> createPayment(@RequestBody Payment payment) {
         return ResponseEntity.ok().body(paymentService.createPayment(payment));
+    }
+
+    @GetMapping(path = "payments")
+    public ResponseEntity<Object> getPayments() {
+        return ResponseEntity.ok().body(paymentService.getPayments());
+    }
+
+    @GetMapping(path = "payments/{id}")
+    public ResponseEntity<Object> getPayment(@PathVariable("id") Long id) {
+        return ResponseEntity.ok().body(paymentService.getPayment(id));
     }
 }
