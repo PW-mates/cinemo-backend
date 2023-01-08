7ac96c126de318105cf9c1fef0ee0c885ba90c2f
Truong Giang Do <73480323+giangh14cqt@users.noreply.github.com>
2023-01-08 18:21:24 +0100

    Get tickettypes (#68)

--- src/main/java/com/pw/cinema/ticket_type/TicketTypeController.java
+++ src/main/java/com/pw/cinema/ticket_type/TicketTypeController.java
@@ -3,6 +3,7 @@ package com.pw.cinema.ticket_type;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,5 +15,10 @@ public class TicketTypeController {
     @PostMapping(path = "ticket-types")
     public ResponseEntity<Object> createTicketType(@RequestBody TicketType ticketType) {
         return ResponseEntity.ok().body(ticketTypeService.createTicketType(ticketType));
+    }
+
+    @GetMapping(path = "ticket-types")
+    public ResponseEntity<Object> getTicketTypes() {
+        return ResponseEntity.ok().body(ticketTypeService.getTicketTypes());
     }
 }
