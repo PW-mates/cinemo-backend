841a27beda4bfcb4111d39a1cd1236254914185d
Truong Giang Do <giangh14cqt@gmail.com>
2023-01-08 17:59:20 +0100

    Add get payments

--- src/main/java/com/pw/cinema/payment/PaymentService.java
+++ src/main/java/com/pw/cinema/payment/PaymentService.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @Service
 public class PaymentService {
@@ -18,6 +20,27 @@ public class PaymentService {
         resp.put("success", true);
         resp.put("data", savedPayment);
         resp.put("message", "Successful create payment");
+        return resp;
+    }
+
+    public Object getPayments() {
+        List<Payment> savedPayments = paymentRepository.findAll();
+        Map<String, Object> resp = new HashMap<>();
+        resp.put("success", true);
+        resp.put("data", savedPayments);
+        resp.put("message", "Successful get payments");
+        return resp;
+    }
+
+    public Object getPayment(Long id) {
+        Payment savedPayment = paymentRepository.findById(id)
+                .orElseThrow(() ->
+                    new NoSuchElementException("Payment with id not found")
+                );
+        Map<String, Object> resp = new HashMap<>();
+        resp.put("success", true);
+        resp.put("data", savedPayment);
+        resp.put("message", "Successful get payment");
         return resp;
     }
 }
