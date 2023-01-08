5d2b6d3e5dd6c50022847fc440f553a8c3b6d8ac
Truong Giang Do <73480323+giangh14cqt@users.noreply.github.com>
2023-01-08 18:15:13 +0100

    create tickettype (#67)

--- src/main/java/com/pw/cinema/ticket_type/TicketTypeController.java
+++ src/main/java/com/pw/cinema/ticket_type/TicketTypeController.java
@@ -1,10 +1,18 @@
 package com.pw.cinema.ticket_type;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class TicketTypeController {
     @Autowired
     TicketTypeService ticketTypeService;
+
+    @PostMapping(path = "ticket-types")
+    public ResponseEntity<Object> createTicketType(@RequestBody TicketType ticketType) {
+        return ResponseEntity.ok().body(ticketTypeService.createTicketType(ticketType));
+    }
 }
--- src/main/java/com/pw/cinema/ticket_type/TicketTypeService.java
+++ src/main/java/com/pw/cinema/ticket_type/TicketTypeService.java
@@ -1,10 +1,29 @@
 package com.pw.cinema.ticket_type;
 
+import com.pw.cinema.seat_type.SeatTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class TicketTypeService {
     @Autowired
     TicketTypeRepository ticketTypeRepository;
+    @Autowired
+    SeatTypeRepository seatTypeRepository;
+
+    public Object createTicketType(TicketType ticketType) {
+        ticketType.setSeatType(
+                seatTypeRepository.findById(ticketType.getSeatType().getId())
+                        .orElseThrow(()
+                                -> new IllegalStateException("SeatType doesn't exist")));
+        TicketType savedTicketType = ticketTypeRepository.save(ticketType);
+        Map<String, Object> resp = new HashMap<>();
+        resp.put("success", true);
+        resp.put("data", savedTicketType);
+        resp.put("message", "Successful create room");
+        return resp;
+    }
 }
